a529428799b6af081172f349b2feec4d9ad8e2b3
Marc jun Landolt <mail@marclandolt.ch>
2018-06-27 23:21:45 +0200

    fixed some minor things and the order of the helperfunction

--- offCanvasMenu.js
+++ offCanvasMenu.js
@@ -2,13 +2,13 @@ var open=false;
 
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+    //document.getElementById("main").style.marginLeft = "250px";
 
 }
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
+    //document.getElementById("main").style.marginLeft= "0";
 }
 
 
